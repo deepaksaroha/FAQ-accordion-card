3523038f08cd71bb5cbb18acc4dee62e7ca1cecb
deepaksaroha <deepaksaroha1993@gmail.com>
2023-03-15 23:50:32 +0530

    as

---
+++ index.js
@@ -0,0 +1,27 @@
+window.onload = () => {
+
+    const faqs = [{
+        q: 'How many team members can I invite?',
+        a: 'You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.'
+    },
+    {
+        q: 'What is the maximum file upload size?',
+        a: 'No more than 2GB. All files in your account must fit your allotted storage space.'
+    },
+    {
+        q: 'How do I reset my password?',
+        a: 'Click “Forgot password” from the login page or “Change password” from your profile page.A reset link will be emailed to you.'
+    },
+    {
+        q: 'Can I cancel my subscription?',
+        a: 'Yes! Send us a message and we’ll process your request no questions asked.'
+    },
+    {
+        q: 'Do you provide additional support?',
+        a: 'Chat and email support is available 24/7. Phone lines are open during normal business hours.'
+    }]
+    
+    const get1Faq = () => {
+
+    }
+}
