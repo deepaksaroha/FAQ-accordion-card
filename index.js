c2f8f9289fd2ed3e7758615cd09af9e9700e8310
deepaksaroha <deepaksaroha1993@gmail.com>
2023-03-16 18:33:18 +0530

    submission

--- index.js
+++ index.js
@@ -21,7 +21,50 @@ window.onload = () => {
         a: 'Chat and email support is available 24/7. Phone lines are open during normal business hours.'
     }]
     
-    const get1Faq = () => {
+    const get1Faq = (q, a) => {
+        const topDiv = document.createElement('div');
+        const queDiv = document.createElement('div');
+        queDiv.classList.add('que');
 
+        const queP = document.createElement('p');
+        queP.innerText = q;
+        queDiv.appendChild(queP);
+
+        const aroDiv = document.createElement('div');
+        const aroImg = document.createElement('img');
+        aroImg.setAttribute('src', './images/icon-arrow-down.svg');
+        aroDiv.appendChild(aroImg);
+        queDiv.appendChild(aroDiv);
+        topDiv.appendChild(queDiv);
+        
+        const ansP = document.createElement('p');
+        ansP.innerText = a;
+        ansP.classList.add('ans', 'anshid');
+        topDiv.appendChild(ansP);
+
+        queDiv.addEventListener('click', ()=>{
+            if(ansP.classList.contains('anshid')){
+                ansP.classList.remove('anshid');
+                ansP.classList.add('ansvis');
+                aroImg.classList.add('arup');
+                queP.classList.add('que-open');
+            }else{                
+                ansP.classList.remove('ansvis');
+                ansP.classList.add('anshid');
+                aroImg.classList.remove('arup');
+                queP.classList.remove('que-open');
+            }
+        })
+        
+        return topDiv;
     }
+
+    const faqSec = document.getElementById('faqsec');
+    for(let i=0; i<faqs.length; i++){
+        let faq = get1Faq(faqs[i].q, faqs[i].a);
+        faqSec.appendChild(faq);
+        const hr = document.createElement('hr');
+        faqSec.appendChild(hr);
+    }
+
 }
